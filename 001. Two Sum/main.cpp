0445f47c2e40cb54da876e044c504d9788fc2ba3
Anch <1481988258@qq.com>
2017-04-05 15:23:55 +0800

    缩进为4

--- 001. Two Sum/main.cpp
+++ 001. Two Sum/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main() {
 	
-	Solution s;
-	vector <int> nums = { 1, 2, 3, 4, 5 };
-	vector <int> res = s.twoSum(nums, 5);
+    Solution s;
+    vector <int> nums = { 1, 2, 3, 4, 5 };
+    vector <int> res = s.twoSum(nums, 5);
 
-	for (auto val : res)
-	{
-		cout << val << endl;
-	}
+    for (auto val : res)
+    {
+        cout << val << endl;
+    }
 
-	return 0;
+    return 0;
 }
