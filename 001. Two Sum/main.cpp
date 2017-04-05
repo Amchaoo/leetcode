83d130fa19d4d57b71c962975df3b043e01c6d25
Anch <1481988258@qq.com>
2017-04-05 11:31:20 +0800

    add 001 solution

---
+++ 001. Two Sum/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		int n = nums.size();
+		map<int, int> map;
+		vector<int> ans;
+		for (int i = 0; i < n; i++) {
+			int d = target - nums[i];
+			if (map.find(d) != map.end()) {
+				ans.push_back(map[d]);
+				ans.push_back(i);
+				return ans;
+			}
+			map[nums[i]] = i;
+		}
+	}
+};
